6e8f93534bd3511830630b147a3ecb9f7e27dd7c
Caracaș Radu Nicolae <caracasradu11@gmail.com>
2023-12-28 18:30:11 +0200

    Projects page done

--- client/pages/portofoliu/index.js
+++ client/pages/portofoliu/index.js
@@ -2,6 +2,30 @@ import React from "react";
 import styles from "../../styles/projects/projects.module.scss";
 import ProjectCard from "./project-card";
 function ProjectsPage() {
+	const projects = [
+		{
+			title: "Project 1",
+			description:
+				"Găsește talentul perfect pentru proiectul tău! Echipa noastră specializată în casting te ajută să selectezi actorii și modelele ideale pentru orice tip de producție, fie că este vorba de filme, reclame sau proiecte fotografice. Avem o bază de date extinsă și variată, garantând potrivirea perfectă pentru viziunea ta artistică.",
+			type: "comercial",
+			imageUrl: "/images/projects/projects.jpg"
+		},
+		{
+			title: "Project 2",
+			description:
+				"Găsește talentul perfect pentru proiectul tău! Echipa noastră specializată în casting te ajută să selectezi actorii și modelele ideale pentru orice tip de producție, fie că este vorba de filme, reclame sau proiecte fotografice. Avem o bază de date extinsă și variată, garantând potrivirea perfectă pentru viziunea ta artistică.",
+			type: "comercial",
+			imageUrl: "/images/projects/projects.jpg"
+		},
+		{
+			title: "Project 3",
+			description:
+				"Găsește talentul perfect pentru proiectul tău! Echipa noastră specializată în casting te ajută să selectezi actorii și modelele ideale pentru orice tip de producție, fie că este vorba de filme, reclame sau proiecte fotografice. Avem o bază de date extinsă și variată, garantând potrivirea perfectă pentru viziunea ta artistică.",
+			type: "comercial",
+			imageUrl: "/images/projects/projects.jpg"
+		}
+	];
+
 	return (
 		<>
 			<div className={styles.heading_text}>
@@ -14,11 +38,17 @@ function ProjectsPage() {
 					garantând potrivirea perfectă pentru viziunea ta artistică.
 				</p>
 			</div>
-			<ProjectCard
-				title="Project1"
-				description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget"
-				imageUrl="/images/carousel/Carousel1.jpg"
-			/>
+			{projects.map((project, index) => (
+				<ProjectCard
+					className={styles.project}
+					key={project.id}
+					title={project.title}
+					description={project.description}
+					type={project.type}
+					imageUrl={project.imageUrl}
+					reverse={index % 2 !== 0} // Pass a prop to determine orientation
+				/>
+			))}
 		</>
 	);
 }
--- client/pages/portofoliu/project-card.js
+++ client/pages/portofoliu/project-card.js
@@ -3,9 +3,13 @@ import styles from "../../styles/projects/ProjectCard.module.scss";
 import Image from "next/image";
 import Frame from "../../public/images/projects/Frame.svg";
 
-const ProjectCard = ({ title, description, imageUrl }) => {
+const ProjectCard = ({ title, description, type, imageUrl, reverse }) => {
+	const cardClasses = reverse
+		? `${styles.projectCard} ${styles.projectCardReverse}`
+		: styles.projectCard;
+
 	return (
-		<div className={styles.projectCard}>
+		<div className={cardClasses}>
 			<div className={styles.imageWrapper}>
 				<Image
 					src={Frame}
@@ -14,11 +18,13 @@ const ProjectCard = ({ title, description, imageUrl }) => {
 				<img
 					src={imageUrl}
 					alt={title}
+					className={styles.projectImage}
 				/>
 			</div>
 			<div className={styles.textContent}>
 				<h2 className={styles.projectTitle}>{title}</h2>
 				<p className={styles.projectDescription}>{description}</p>
+				<h3 className={styles.projectType}>{type}</h3>
 			</div>
 		</div>
 	);
