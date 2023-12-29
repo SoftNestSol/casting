255a82091761d3a4e7f794210acda335ae082a75
Caracaș Radu Nicolae <caracasradu11@gmail.com>
2023-12-29 15:05:38 +0200

    spline fix

--- client/pages/index.js
+++ client/pages/index.js
@@ -5,8 +5,8 @@ import CTA from "./cta-section";
 import styles from "../styles/landing-page/landing-page.module.scss";
 import React, { Suspense } from "react";
 import Link from "next/link";
-const Spline = React.lazy(() => import("@splinetool/react-spline"));
-
+// const Spline = React.lazy(() => import("@splinetool/react-spline"));
+import Spline from "@splinetool/react-spline";
 export default function LandingPage() {
 	return (
 		<div>
@@ -29,7 +29,12 @@ export default function LandingPage() {
 					</div>
 
 					<div className={styles.button_container}>
-						<Link className={styles.button} href = "/register">APLICĂ ACUM</Link>
+						<Link
+							className={styles.button}
+							href="/register"
+						>
+							APLICĂ ACUM
+						</Link>
 					</div>
 					<div className={styles.hero_text}>
 						<h1>
