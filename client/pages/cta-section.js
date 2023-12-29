dbb563fade9aa1449c8e78931bed3d2a74155c48
Andrei Voicu <andrei.voicu110@icloud.com>
2023-12-30 01:38:43 +0200

    Fix

--- client/pages/cta-section.js
+++ client/pages/cta-section.js
@@ -7,16 +7,21 @@ import Link from "next/link";
 const CTA = () => {
 	return (
 		<div className={styles.sectionContainer}>
-			<div className={styles.textContent}>
-				<p className={styles.description}>
-					Suntem o agenție proaspătă și ne dorim să ne diferențiem prin comunicarea deschisă cu
-					viitorii noștri actori. Aici găsești proiectele perfecte pentru tine și poți discuta liber
-					despre nevoile și dorințele tale.
-				</p>
-				<Link className={styles.applyButton} href = "/register">APLICĂ ACUM</Link>
-			</div>
-			<div className={styles.imageContainer}>
-				<div className={styles.imageFrame}>
+			<div className={styles.wrapper}>
+				<div className={styles.textContent}>
+					<p className={styles.description}>
+						Suntem o agenție proaspătă și ne dorim să ne diferențiem prin comunicarea deschisă cu
+						viitorii noștri actori. Aici găsești proiectele perfecte pentru tine și poți discuta
+						liber despre nevoile și dorințele tale.
+					</p>
+					<Link
+						className={styles.applyButton}
+						href="/register"
+					>
+						APLICĂ ACUM
+					</Link>
+				</div>
+				<div className={styles.imageContainer}>
 					<Image
 						alt="frame"
 						src={Frame}
