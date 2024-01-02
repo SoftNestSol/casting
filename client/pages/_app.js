f8d9fb616db54854b3ac6ff6a35b918ca9e551dd
Caracaș Radu Nicolae <caracasradu11@gmail.com>
2024-01-02 23:22:05 +0200

    loader

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import { AuthContextProvider } from "../contexts/auth.context";
 import { CastingsContextProvider } from "../contexts/castings.context";
 import Navbar from "../components/navbar";
@@ -9,25 +9,16 @@ import "../styles/globals.scss";
 
 const App = ({ Component, pageProps }) => {
 	const router = useRouter();
-	const [isSplineLoaded, setSplineLoaded] = useState(false);
-	const onLoadCalled = useRef(false);
+	const [showLoading, setShowLoading] = useState(true);
 	const isLandingPage = router.pathname === "/";
 
 	useEffect(() => {
-		console.log("Mounting Spline component");
-		return () => {
-			console.log("Unmounting Spline component");
-		};
-	}, []);
+		const timer = setTimeout(() => {
+			setShowLoading(false);
+		}, 2500); // Set this to your desired loading message display time
 
-	const onLoadFunction = () => {
-		console.log("Spline onLoad triggered");
-		if (!onLoadCalled.current) {
-			setSplineLoaded(true);
-			onLoadCalled.current = true;
-			console.log("Spline is set to loaded");
-		}
-	};
+		return () => clearTimeout(timer);
+	}, []);
 
 	return (
 		<AuthContextProvider>
@@ -35,14 +26,9 @@ const App = ({ Component, pageProps }) => {
 				<Navbar />
 				<div className="main-content">
 					<div className={`spline ${isLandingPage ? "" : "hidden"}`}>
-						{!isSplineLoaded && (
-							<div className="spline-loading">Loading 3D content, please wait...</div>
-						)}
-
-						<Spline
-							scene="https://prod.spline.design/r0ZUySqcJPwMFSGy/scene.splinecode"
-							onLoad={onLoadFunction}
-						/>
+						{showLoading && <div className="loader"></div>}
+						{/* Spline component is always rendered */}
+						<Spline scene="https://prod.spline.design/r0ZUySqcJPwMFSGy/scene.splinecode" />
 					</div>
 					<Component {...pageProps} />
 				</div>
