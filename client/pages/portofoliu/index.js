d3a3cbd6d54dcca67b1b5c60cadcc90753e69477
Caracaș Radu Nicolae <caracasradu11@gmail.com>
2024-01-04 01:01:57 +0200

    content

--- client/pages/portofoliu/index.js
+++ client/pages/portofoliu/index.js
@@ -4,25 +4,18 @@ import ProjectCard from "./project-card";
 function ProjectsPage() {
 	const projects = [
 		{
-			title: "Project 1",
+			title: "Capcana Diavolului Miliardar",
 			description:
-				"Găsește talentul perfect pentru proiectul tău! Echipa noastră specializată în casting te ajută să selectezi actorii și modelele ideale pentru orice tip de producție, fie că este vorba de filme, reclame sau proiecte fotografice. Avem o bază de date extinsă și variată, garantând potrivirea perfectă pentru viziunea ta artistică.",
-			type: "comercial",
-			imageUrl: "/images/projects/projects.jpg"
+				"O mini-serie captivantă, filmată în colaborare cu o companie din China. Povestea explorează relațiile între două familii ale căror băieți au crescut împreună în copilărie și care se află acum într-un triunghi amoros complicat. Îndrăgostiți de aceeași fată, relația devine și mai tensionată odată ce o crimă are loc, aducând un element surprinzător în acest triunghi amoros clasic.Este rezultatul unei colaborări fascinante cu o firmă din China, aducând în fața publicului o producție cinematografică ce traversează granițele culturale și aduce laolaltă elemente dramatice și misterioase.",
+			type: "Serial",
+			imageUrl: "/images/projects/1.jpg"
 		},
 		{
-			title: "Project 2",
+			title: "Insărcinată după respingerea masculului Alfa",
 			description:
-				"Găsește talentul perfect pentru proiectul tău! Echipa noastră specializată în casting te ajută să selectezi actorii și modelele ideale pentru orice tip de producție, fie că este vorba de filme, reclame sau proiecte fotografice. Avem o bază de date extinsă și variată, garantând potrivirea perfectă pentru viziunea ta artistică.",
-			type: "comercial",
-			imageUrl: "/images/projects/projects.jpg"
-		},
-		{
-			title: "Project 3",
-			description:
-				"Găsește talentul perfect pentru proiectul tău! Echipa noastră specializată în casting te ajută să selectezi actorii și modelele ideale pentru orice tip de producție, fie că este vorba de filme, reclame sau proiecte fotografice. Avem o bază de date extinsă și variată, garantând potrivirea perfectă pentru viziunea ta artistică.",
-			type: "comercial",
-			imageUrl: "/images/projects/projects.jpg"
+				"Povestea se învârte în jurul personajului feminin principal, cea mai tânără dintr-o familie de vârcolaci, având o sora mai mare iubită de toți. Simțindu-se marginalizată deoarece nu se poate transforma în vârcolac ca ceilalți membri ai familiei, ea călătorește într-o lume mistică.Această poveste SF complexă este transpusă într-o miniserie captivantă, filmată într-un timp scurt, unde creativitatea și improvizația decorului au fost realizate în echipă, atât de actori, cât și de echipa de producție. Este o poveste cu adevărat provocatoare, care explorează elemente fantastice și umane într-un mod captivant.",
+			type: "Serial",
+			imageUrl: "/images/projects/2.jpg"
 		}
 	];
 
@@ -32,10 +25,11 @@ function ProjectsPage() {
 				<h1>Proiectele</h1>
 				<h1>Noastre</h1>
 				<p>
-					Găsește talentul perfect pentru proiectul tău! Echipa noastră specializată în casting te
-					ajută să selectezi actorii și modelele ideale pentru orice tip de producție, fie că este
-					vorba de filme, reclame sau proiecte fotografice. Avem o bază de date extinsă și variată,
-					garantând potrivirea perfectă pentru viziunea ta artistică.
+					Talentul apare în fiecare persoană, indiferent de vârstă, sex, etnie sau naționalitate.
+					Tot ce trebuie să facem este să privim cu atenție și să-l îmbrățișăm. Asta facem la
+					MyCasting. Găsim talente de orice fel pentru proiecte creative. Oferim o colaborare
+					fiabilă, eficientă și de succes pentru studiouri și un spațiu creativ, sigur pentru actori
+					să experimenteze, să învețe și să-și atingă obiectivele.
 				</p>
 			</div>
 			{projects.map((project, index) => (
