efe8ca7c27cdaf797f3fefd88daac04d89ba4c99
Caracaș Radu Nicolae <caracasradu11@gmail.com>
2024-01-04 17:54:22 +0200

    mobile image

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -6,11 +6,12 @@ import Spline from "@splinetool/react-spline";
 import { useRouter } from "next/router";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/globals.scss";
+import Head from "next/head";
 import { DashboardContextProvider } from "../contexts/dashboard.context";
-
 const App = ({ Component, pageProps }) => {
 	const router = useRouter();
 	const [showLoading, setShowLoading] = useState(true);
+	const [isMobile, setIsMobile] = useState(false);
 	const isLandingPage = router.pathname === "/";
 
 	useEffect(() => {
@@ -18,35 +19,76 @@ const App = ({ Component, pageProps }) => {
 			setShowLoading(false);
 		}, 2500);
 
-		return () => clearTimeout(timer);
+		// Check for mobile screen size
+		const checkMobile = () => {
+			setIsMobile(window.innerWidth < 768); // or any other width you consider as mobile
+		};
+
+		// Set initial value
+		checkMobile();
+
+		// Add resize listener
+		window.addEventListener("resize", checkMobile);
+
+		// Cleanup
+		return () => {
+			clearTimeout(timer);
+			window.removeEventListener("resize", checkMobile);
+		};
 	}, []);
-
 	return (
-		<AuthContextProvider>
-			<CastingsContextProvider>
-				<DashboardContextProvider>
-					{showLoading && (
-						<>
-							<div
-								className={`loader ${showLoading ? "loader_active" : ""}`}
-							></div>
-							<div className="loader_background"></div>
-						</>
-					)}
+		<>
+			<Head>
+				<title>Castings</title>
+				<meta
+					name="viewport"
+					content="initial-scale=1.0, width=device-width"
+				/>
+				<link
+					rel="icon"
+					href="/favicon.ico"
+				/>
+				<link
+					rel="stylesheet"
+					href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;600&display=swap"
+				/>
+			</Head>
 
-					<Navbar />
+			<AuthContextProvider>
+				<CastingsContextProvider>
+					<DashboardContextProvider>
+						{showLoading && (
+							<>
+								<div
+									className={`loader ${showLoading ? "loader_active" : ""}`}
+								></div>
+								<div className="loader_background"></div>
+							</>
+						)}
 
-					<div className="main-content">
-						<div className={`spline ${isLandingPage ? "" : "hidden"}`}>
-							{/* Spline component is always rendered */}
-							<Spline scene="https://prod.spline.design/r0ZUySqcJPwMFSGy/scene.splinecode" />
+						<Navbar />
+
+						<div className="main-content">
+							{isLandingPage && (
+								<div className={`spline ${isLandingPage ? "" : "hidden"}`}>
+									{/* Conditional rendering based on screen size */}
+									{isMobile ? (
+										<img
+											src="/images/splineImg.png"
+											alt="Mobile Image"
+										/>
+									) : (
+										<Spline scene="https://prod.spline.design/r0ZUySqcJPwMFSGy/scene.splinecode" />
+									)}
+								</div>
+							)}
+
+							<Component {...pageProps} />
 						</div>
-
-						<Component {...pageProps} />
-					</div>
-				</DashboardContextProvider>
-			</CastingsContextProvider>
-		</AuthContextProvider>
+					</DashboardContextProvider>
+				</CastingsContextProvider>
+			</AuthContextProvider>
+		</>
 	);
 };
 
