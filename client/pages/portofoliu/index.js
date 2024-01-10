839e7c2857f624adce2fd8c17757b0d1936663ef
Caracaș Radu Nicolae <caracasradu11@gmail.com>
2024-01-10 14:41:48 +0200

    galerie la portofoliiu

--- client/pages/portofoliu/index.js
+++ client/pages/portofoliu/index.js
@@ -9,14 +9,24 @@ const projects = [
 		description:
 			"O mini-serie captivantă, filmată în colaborare cu o companie din China. Povestea explorează relațiile între două familii ale căror băieți au crescut împreună în copilărie și care se află acum într-un triunghi amoros complicat. Îndrăgostiți de aceeași fată, relația devine și mai tensionată odată ce o crimă are loc, aducând un element surprinzător în acest triunghi amoros clasic.Este rezultatul unei colaborări fascinante cu o firmă din China, aducând în fața publicului o producție cinematografică ce traversează granițele culturale și aduce laolaltă elemente dramatice și misterioase.",
 		type: "Serial",
-		imageUrl: "/images/projects/1.jpg"
+		imageUrls: [
+			"/images/projects/1/1.jpg",
+			"/images/projects/1/2.jpg",
+			"/images/projects/1/3.jpg",
+			"/images/projects/1/4.jpg"
+		]
 	},
 	{
 		title: "Insărcinată după respingerea masculului Alfa",
 		description:
 			"Povestea se învârte în jurul personajului feminin principal, cea mai tânără dintr-o familie de vârcolaci, având o sora mai mare iubită de toți. Simțindu-se marginalizată deoarece nu se poate transforma în vârcolac ca ceilalți membri ai familiei, ea călătorește într-o lume mistică.Această poveste SF complexă este transpusă într-o miniserie captivantă, filmată într-un timp scurt, unde creativitatea și improvizația decorului au fost realizate în echipă, atât de actori, cât și de echipa de producție. Este o poveste cu adevărat provocatoare, care explorează elemente fantastice și umane într-un mod captivant.",
 		type: "Serial",
-		imageUrl: "/images/projects/2.jpg"
+		imageUrls: [
+			"/images/projects/2/1.jpg",
+			"/images/projects/2/2.jpg",
+			"/images/projects/2/3.jpg",
+			"/images/projects/2/4.jpg"
+		]
 	}
 ];
 
@@ -43,7 +53,7 @@ const Projects = () => {
 						title={project.title}
 						description={project.description}
 						type={project.type}
-						imageUrl={project.imageUrl}
+						imageUrls={project.imageUrls}
 						reverse={index % 2 !== 0}
 					/>
 				</div>
--- client/pages/portofoliu/project-card.js
+++ client/pages/portofoliu/project-card.js
@@ -1,37 +1,85 @@
-import React from "react";
-import styles from "../../styles/projects/ProjectCard.module.scss";
+import React, { useState, useEffect } from "react";
+import Modal from "react-modal";
 import Image from "next/image";
+import styles from "../../styles/projects/ProjectCard.module.scss";
 import Frame from "../../public/images/projects/Frame.svg";
 
-const ProjectCard = ({ title, description, type, imageUrl, reverse }) => {
+Modal.setAppElement("#__next"); // Ensure accessibility for the modal
+
+const ProjectCard = ({ title, description, type, imageUrls, reverse }) => {
+	const [modalIsOpen, setIsOpen] = useState(false);
+
+	useEffect(() => {
+		Modal.setAppElement("#__next");
+	}, []);
+
+	const toggleModal = () => {
+		setIsOpen(!modalIsOpen);
+	};
+
 	const cardClasses = reverse
 		? `${styles.projectCard} ${styles.projectCardReverse}`
 		: styles.projectCard;
 
 	return (
-		<div className={cardClasses}>
-			<div className={styles.imageWrapper}>
-				<Image
-					alt={title}
-					src={Frame}
-					className={styles.decoration}
-					width={100}
-					height={100}
-				/>
-				<Image
-					src={imageUrl}
-					alt={title}
-					className={styles.projectImage}
-					width={500}
-					height={500}
-				/>
+		<>
+			<div
+				className={cardClasses}
+				onClick={toggleModal}
+			>
+				<div className={styles.imageWrapper}>
+					<Image
+						alt={title}
+						src={Frame}
+						className={styles.decoration}
+						width={100}
+						height={100}
+					/>
+					{/* Display the first image or a placeholder if no images are provided */}
+					<Image
+						src={
+							imageUrls.length > 0 ? imageUrls[0] : "/path/to/placeholder.jpg"
+						}
+						alt={title}
+						className={styles.projectImage}
+						width={500}
+						height={500}
+					/>
+				</div>
+				<div className={styles.textContent}>
+					<h2 className={styles.projectTitle}>{title}</h2>
+					<p className={styles.projectDescription}>{description}</p>
+					<h3 className={styles.projectType}>{type}</h3>
+				</div>
 			</div>
-			<div className={styles.textContent}>
-				<h2 className={styles.projectTitle}>{title}</h2>
-				<p className={styles.projectDescription}>{description}</p>
-				<h3 className={styles.projectType}>{type}</h3>
-			</div>
-		</div>
+
+			<Modal
+				isOpen={modalIsOpen}
+				onRequestClose={toggleModal}
+				className={styles.modal}
+				overlayClassName={styles.overlay}
+			>
+				<button
+					onClick={toggleModal}
+					className={styles.closeButton}
+				>
+					&times;
+				</button>{" "}
+				{/* Close button */}
+				<div className={styles.modalContent}>
+					{imageUrls.map((url, index) => (
+						<Image
+							key={index}
+							src={url}
+							alt={`${title} image ${index + 1}`}
+							layout="responsive"
+							width={500}
+							height={500}
+						/>
+					))}
+				</div>
+			</Modal>
+		</>
 	);
 };
 
