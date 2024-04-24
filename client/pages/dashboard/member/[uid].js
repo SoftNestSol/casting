6eaf0f95d9ac0fde50883dc54efc3e8184d21559
Iordy <andrei.iordache2017@gmail.com>
2024-04-24 13:33:27 +0300

    upload photos / admin

--- client/pages/dashboard/member/[uid].js
+++ client/pages/dashboard/member/[uid].js
@@ -1,17 +1,31 @@
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { doc, getDoc } from "firebase/firestore";
 
 import { db } from "../../../config/firebase";
 import { useDashboardContext } from "../../../contexts/dashboard.context";
+import { storage } from "../../../config/firebase";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { resizedName } from "../../../contexts/auth.context";
+import { updateDoc } from "firebase/firestore";
+import { useRef } from "react";
 
-import styles from "../../../styles/profile/profile.module.scss";
-import photoStyles from "../../../styles/dashboard/photo.module.scss";
+import photoStyles from "../../../Styles/dashboard/photo.module.scss";
 
 const MemberPage = () => {
 	const [user, setUser] = useState(null);
 	const router = useRouter();
 	const [loading, setLoading] = useState(true);
+	const [profileData, setProfileData] = useState({
+		photos: [],
+		files: []
+	});
+
+	const openFilePicker = () => {
+		inputRef.current.click();
+	};
+
+	const inputRef = useRef(null);
 
 	const { ComputeAge } = useDashboardContext();
 
@@ -37,6 +51,10 @@ const MemberPage = () => {
 		getUserData(uid)
 			.then((userData) => {
 				setUser(userData);
+				setProfileData((prevState) => ({
+					...prevState,
+					photos: userData.photos
+				}));
 			})
 			.catch((error) => {
 				console.error("Error fetching user data: ", error);
@@ -50,12 +68,66 @@ const MemberPage = () => {
 		return <div>No user data found.</div>;
 	}
 
+	const handleFileInputChange = (event) => {
+		[...event.target.files].forEach((file) => {
+			if (!file.name.match(/\.(jpg|jpeg|png)$/))
+				return alert("Fisierul nu este o imagine!");
+
+			const reader = new FileReader();
+
+			reader.onload = () =>
+				setProfileData((prevState) => ({
+					...prevState,
+					photos: [reader.result, ...prevState.photos],
+					files: [file, ...prevState.files]
+				}));
+
+			reader.readAsDataURL(file);
+		});
+	};
+
+	const deletePhoto = async (photoUrl) => {
+		setProfileData((prevState) => ({
+			...prevState,
+			photos: prevState.photos.filter((photo) => photo !== photoUrl)
+		}));
+
+		const userRef = doc(db, "users", user.uid);
+		const newPhotos = user.photos.filter((photo) => photo !== photoUrl);
+
+		await updateDoc(userRef, {
+			photos: newPhotos
+		});
+
+		setUser((prevState) => ({
+			...prevState,
+			photos: newPhotos
+		}));
+	};
+
+	const uploadPhotos = async () => {
+		const promises = profileData.files.map(async (file) => {
+			const photoRef = ref(storage, `photos/${router.query.uid}/${file.name}`);
+			const snapshot = await uploadBytes(photoRef, file);
+			const url = await getDownloadURL(snapshot.ref);
+			return resizedName(url);
+		});
+
+		const urls = await Promise.all(promises);
+
+		const userRef = doc(db, "users", user.uid);
+		await updateDoc(userRef, {
+			photos: [...user.photos, ...urls]
+		});
+		router.reload();
+	};
+
 	return (
-		<div className={styles.wrapper}>
-			<div className={styles.container}>
-				<div className={styles.content}>
+		<div className={photoStyles.wrapper}>
+			<div className={photoStyles.container}>
+				<div className={photoStyles.content}>
 					<h1>Profilul {user.name}</h1>
-					<div className={styles.fields}>
+					<div className={photoStyles.fields}>
 						<p>
 							<strong>Nume:</strong> {user.name}
 						</p>
@@ -98,22 +170,57 @@ const MemberPage = () => {
 							<strong>Descriere:</strong> {user.description}
 						</p>
 
-						{user.photos && user.photos.length > 0 && (
+						<div>
 							<div className={photoStyles.photos}>
-								{user.photos.map((photo, index) => (
-									<div
-										className={styles.photo}
-										key={index}
-									>
-										<img
-											alt={`Photo ${index + 1}`}
-											src={photo}
+								{profileData.photos.map((photoUrl, index) => (
+									<div>
+										<div
+											key={index}
 											className={photoStyles.photo}
-										/>
+										>
+											<img
+												className={photoStyles.photo}
+												src={photoUrl}
+												alt={`Photo ${index + 1}`}
+											/>
+										</div>
+										<button
+											className={photoStyles.removeButton}
+											onClick={() => deletePhoto(photoUrl)}
+										>
+											Sterge
+										</button>
 									</div>
 								))}
 							</div>
-						)}
+							<div className={photoStyles.buttonsContainer}>
+								<div>
+									<button
+										type="button"
+										onClick={openFilePicker}
+										className={photoStyles.uploadButton}
+									>
+										Alege pozele noi
+									</button>
+
+									<input
+										ref={inputRef}
+										id="photos"
+										name="photos"
+										onChange={handleFileInputChange}
+										type="file"
+										multiple
+										style={{ display: "none" }}
+									/>
+								</div>
+								<button
+									className={photoStyles.uploadButton}
+									onClick={uploadPhotos}
+								>
+									Aplica Schimbari
+								</button>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
